0bf2e1860283b157f2f7aef0dee2327740c78c74
Dan Falcon <dan_1196@yahoo.com>
2024-09-20 16:49:11 +0100

    Make values computed

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-  import { ref } from 'vue'
+  import { ref, computed } from 'vue'
   import socksGreenImage from './assets/images/socks_green.jpeg'
   import socksBlueImage from './assets/images/socks_blue.jpeg'
 
@@ -9,21 +9,36 @@
   // Wrapping ref for Reactivity (useState)
   // Use reactive if using object with bunch of related data
   const product = ref('Socks')
-  const image = ref(socksGreenImage)
-  const inStock = ref(false)
+  const brand = ref('Vue Mastery')
+
+  const selectedVariant = ref(0)
+
+  // Computed = values that need to compute, it update automatically
+  // Watch =  watch and callback if a specific updated
+  // WatchEffect = watch on all changes, better on any state change
+  const image = computed(() => {
+    return variants.value[selectedVariant.value].image
+  })
+  const inStock = computed(() => {
+    return variants.value[selectedVariant.value].quantity > 0
+  })
 
   const details = ref(['50% cotton', '50% wool', '20% polyester'])
 
   const variants = ref([
-    { id: 2234, color: 'green', image: socksGreenImage },
-    { id: 2235, color: 'blue', image: socksBlueImage },
+    { id: 2234, color: 'green', image: socksGreenImage, quantity: 50 },
+    { id: 2235, color: 'blue', image: socksBlueImage, quantity: 0 },
   ])
 
   const cart = ref(0)
   const addToCart = () => cart.value += 1
 
-  const updateImage = (variantImage) => {
-    image.value = variantImage
+  const title = computed(() => {
+    return brand.value + ' ' + product.value
+  })
+
+  const updateVariant = (index) => {
+    selectedVariant.value = index
   }
 </script>
 <template>
@@ -37,7 +52,7 @@
       </div>
       <div class="product-info">
         <!-- Equivalent of {} in React -->
-        <h1> {{ product }}</h1>
+        <h1> {{ title }}</h1>
         <!-- Can use v-show = Still rendered. display: none. More performant -->
         <p v-if="inStock">In Stock</p>
         <p v-else>Out of Stock</p>
@@ -46,9 +61,9 @@
         </ul>
         <!-- style="{}" = React's style={{}} -->
         <div
-          v-for="variant in variants"
+          v-for="(variant, index) in variants"
           :key="variant.id"
-          @mouseover="updateImage(variant.image)"
+          @mouseover="updateVariant(index)"
           class="color-circle"
           :style="{ backgroundColor: variant.color }"
         >
