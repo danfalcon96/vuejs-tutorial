fd900e2d64441127f246bb6d52da9950601571a7
Dan Falcon <dan_1196@yahoo.com>
2024-09-20 08:05:54 +0100

    Add "Add to Cart" button and selection of color

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
   import { ref } from 'vue'
   import socksGreenImage from './assets/images/socks_green.jpeg'
+  import socksBlueImage from './assets/images/socks_blue.jpeg'
 
   // Single-File Component === React Hooks
   // Composables === Custom Hooks
@@ -14,12 +15,20 @@
   const details = ref(['50% cotton', '50% wool', '20% polyester'])
 
   const variants = ref([
-    { id: 2234, color: 'green' },
-    { id: 2235, color: 'blue' }
+    { id: 2234, color: 'green', image: socksGreenImage },
+    { id: 2235, color: 'blue', image: socksBlueImage },
   ])
+
+  const cart = ref(0)
+  const addToCart = () => cart.value += 1
+
+  const updateImage = (variantImage) => {
+    image.value = variantImage
+  }
 </script>
 <template>
   <div class="nav-bar"></div>
+  <div class="cart">Cart({{cart}})</div>
   <div class="product-display">
     <div class="product-container">
       <div class="product-image">
@@ -35,9 +44,22 @@
         <ul>
           <li v-for="detail in details">{{ detail }}</li>
         </ul>
-        <div v-for="variant in variants" :key="variant.id">{{ variant.color }}</div>
+        <div
+          v-for="variant in variants"
+          :key="variant.id"
+          @mouseover="updateImage(variant.image)"
+        >
+          {{ variant.color }}
+        </div>
         <!-- If using if and for together, if is always first. -->
         <!-- To make for to be first, put in diff elements or nesting -->
+
+        <button class="button" v-on:click="addToCart">Add to Cart</button>
+        <!-- Can use event modifiers -->
+        <!-- e.g. @click.once = make sure event triggered only once -->
+        <!-- @click.prevent = prevent native behavior. event.preventDefault -->
+        <!-- @click.stop = stop propagating further up -->
+        <!-- @keydown.enter = Enter key keydown -->
       </div>
     </div>
   </div>
