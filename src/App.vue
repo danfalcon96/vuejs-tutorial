8b4e58dc28047cb2468cc215b2df1432999fbb45
Dan Falcon <dan_1196@yahoo.com>
2024-09-20 07:36:20 +0100

    Add image and product info style

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <script setup>
   import { ref } from 'vue'
+  import socksGreenImage from './assets/images/socks_green.jpeg'
+
   // Single-File Component === React Hooks
   // Composables === Custom Hooks
 
@@ -8,5 +10,18 @@
   const product = ref('Socks')
 </script>
 <template>
-  <h1> {{ product }}</h1>
+  <div class="nav-bar"></div>
+  <div class="product-display">
+    <div class="product-container">
+      <div class="product-image">
+        <!-- v-bind = binding data to the variable -->
+
+        <img v-bind:src="socksGreenImage">
+      </div>
+      <div class="product-info">
+        <!-- Equivalent of {} in React -->
+        <h1> {{ product }}</h1>
+      </div>
+    </div>
+  </div>
 </template>
